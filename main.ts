8a2ac5e4ea0c826ef61b3b432d75307745e6891c
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-05-24 23:32:14 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+var firstname = "sadia"
+console.log(firstname)
+
+// 1st way to declare a variable //
+var firstname:string = "sadiakanwal"
+console.log(firstname)
+
+// 2nd way to declare a variable //
+var fathername = "raeesahmed"
+console.log(fathername)
+
+// 3rd way to declare a variable //
+var employeename;
+console.log(employeename)
+
